86634fde4ae79c8fa596c145100f3bfe13af7f9f
PaulDavidowitz <pdavidow@fastmail.fm>
2015-03-10 15:52:53 -0400

    v13

--- client/template.js
+++ client/template.js
@@ -1,6 +1,20 @@
+Session.set("isClick",false);
+
 var currentPieceId = function() {
     return Session.get("currentPieceId");
 };
+
+Template.main.helpers({
+    isClickStart: function() {
+        return Session.get("isClick");
+    }
+});
+Template.main.events({
+    "click #start": function() {
+        debugger;
+        Session.set("isClick",true);
+    }
+});
 
 Template.pieces.helpers({
     isDisabled: function() {
